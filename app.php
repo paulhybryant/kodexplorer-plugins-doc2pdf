ecbf414566a96691ec3832a2b8aa79a6eaff8619
paulhybryant <paulhybryant@gmail.com>
2019-03-11 15:44:33 +0800

    Working

--- app.php
+++ app.php
@@ -15,8 +15,9 @@ class doc2pdfPlugin extends PluginBase{
     }
     public function doc2pdf(){
       $path = urldecode(_DIR($this->in['path']));
-      $result = shell_exec("cd " . dirname($path) . " && soffice --headless --convert-to pdf " . $path . " --outdir " . dirname($path));
-      echo "<script>console.log( 'Run: " . "soffice --headless --convert-to pdf " . $path . " --outdir " . dirname($path) . "' );</script>";
+      $cmd = "HOME=/tmp soffice --headless --invisible --convert-to pdf " . $path . " --outdir " . dirname($path);
+      $result = shell_exec($cmd);
+      echo "<script>console.log( 'Run: " . $cmd . "' );</script>";
       echo "<script>console.log( 'Result: " . $result . "' );</script>";
     }
 }
